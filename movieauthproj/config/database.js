6b0a4b528c58d7c84cc9fc90aef4b3daa109c734
Ath Tripathi <59201258+ath34-tech@users.noreply.github.com>
2022-09-23 20:32:48 +0530

    Update database.js

--- movieauthproj/config/database.js
+++ movieauthproj/config/database.js
@@ -9,5 +9,3 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
-// 375235664597-shar498s5v4ml2tnno0ljkls6vckmcte.apps.googleusercontent.com
-// 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
